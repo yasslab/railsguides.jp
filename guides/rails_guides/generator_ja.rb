5d54bb89060a2dafc4514c740bcb8b77908b6878
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-05-11 12:05:45 +0900

    Move #set_flags_from_environment

--- guides/rails_guides/generator_ja.rb
+++ guides/rails_guides/generator_ja.rb
@@ -2,5 +2,9 @@ require_relative "generator"
 
 module RailsGuides
   class GeneratorJa < Generator
+    def set_flags_from_environment
+      super
+      @dash = ENV['DASH'] == '1'
+    end
   end
 end
